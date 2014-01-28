bab326f3e2d742eea2c7c7049a75f874d7218ef6
james.r.nash@googlemail.com <james.r.nash@googlemail.com@94efd472-7a52-070b-33cc-ba840b30f4b1>
2014-01-28 14:10:41 +0000

    Updated to support 11.1.1.7 and some general clean-up

--- BuildKit/build.cmd
+++ BuildKit/build.cmd
@@ -17,7 +17,7 @@ SET ANT_HOME=%FMW_HOME%\modules\org.apache.ant_1.7.1
 @REM    WORKSPACE		The absolute path to the Hudson workspace
 @REM    BUILD_NUMBER	The Hudson build number
 
-SET WORKSPACE=D:\hudson\jobs\SampleOSB
+SET WORKSPACE=
 SET BUILD_NUMBER=
 
 IF NOT EXIST "%WORKSPACE%" (
@@ -29,31 +29,32 @@ IF NOT EXIST "%WORKSPACE%" (
 
 @REM Hudson would have performed a subversion checkout or update for us. This
 @REM emulates this behaviour based on the svn-projects.properties file.
-FOR /F "eol=# tokens=1,2*" %%i IN (projects.properties) DO (
-	IF EXIST "%WORKSPACE%\%%j" (
-		ECHO Updating dir %%j from %%i
-		@REM assume we have anon access to svn repository
-		svn up "%WORKSPACE%\%%j"
-	) ELSE (
-		ECHO Checkout %%i to dir %%j
-		@REM assume we have anon access to svn repository
-		svn co "%%i" "%WORKSPACE%\%%j"
-	)
-)
+@REM FOR /F "eol=# tokens=1,2*" %%i IN (projects.properties) DO (
+@REM 	IF EXIST "%WORKSPACE%\%%j" (
+@REM 		ECHO Updating dir %%j from %%i
+@REM 		@REM assume we have anon access to svn repository
+@REM 		svn up "%WORKSPACE%\%%j"
+@REM 	) ELSE (
+@REM 		ECHO Checkout %%i to dir %%j
+@REM 		@REM assume we have anon access to svn repository
+@REM 		svn co "%%i" "%WORKSPACE%\%%j"
+@REM 	)
+@REM )
 
 @REM Emulate the Hudson Build number environment var
-IF EXIST build.number (
-	FOR /F "" %%i IN (build.number) DO (
-		SET /A BUILD_NUMBER=%%i+1
-	)
-) ELSE (
-	SET BUILD_NUMBER=1
-)
-ECHO %BUILD_NUMBER% > build.number
-ECHO Build number: %BUILD_NUMBER%
+@REM IF EXIST build.number (
+@REM 	FOR /F "" %%i IN (build.number) DO (
+@REM 		SET /A BUILD_NUMBER=%%i+1
+@REM 	)
+@REM ) ELSE (
+@REM 	SET BUILD_NUMBER=1
+@REM )
+@REM ECHO %BUILD_NUMBER% > build.number
+@REM ECHO Build number: %BUILD_NUMBER%
 
 @REM Invoke the Ant build.xml to initialize the build
-"%ANT_HOME%\bin\ant" -Dconfig.project=OSBSamplesConfig build,deploy
+@REM "%ANT_HOME%\bin\ant" -Dconfig.project=GenieOSBConfig build,deploy
+"%ANT_HOME%\bin\ant" -Dconfig.project=GenieOSBConfig build
 
 
 ENDLOCAL
